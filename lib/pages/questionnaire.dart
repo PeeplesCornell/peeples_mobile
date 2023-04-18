4c221514c869c41d8fc229980969a89395701aea
MayaShenHongyu <hs692@cornell.edu>
2023-04-18 01:58:11 -0400

    add question index navigation

--- lib/pages/questionnaire.dart
+++ lib/pages/questionnaire.dart
@@ -19,7 +19,7 @@ class Questionnaire extends ConsumerWidget {
               debugPrint(snapshot.error.toString());
               return const Text('Something went wrong');
             } else if (snapshot.connectionState == ConnectionState.done) {
-              return QuestionnaireView(data: snapshot.data!);
+              return QuestionnaireView(questions: snapshot.data!);
             } else {
               return const CircularProgressIndicator(
                 valueColor: AlwaysStoppedAnimation(
@@ -35,58 +35,113 @@ class Questionnaire extends ConsumerWidget {
 }
 
 class QuestionnaireView extends StatefulWidget {
-  final List<String> data;
-  const QuestionnaireView({Key? key, required this.data}) : super(key: key);
+  final List<String> questions;
+  const QuestionnaireView({Key? key, required this.questions})
+      : super(key: key);
 
   @override
   State<QuestionnaireView> createState() => _QuestionnaireViewState();
 }
 
 class _QuestionnaireViewState extends State<QuestionnaireView> {
-  final int _index = 0;
-  late final List<String?> _result =
-      List.generate(widget.data.length, (index) => null);
+  int _index = 0;
+  late List<String?> _answers =
+      List.generate(widget.questions.length, (index) => null);
+
+  void _nextPage() {
+    setState(() {
+      _index += 1;
+    });
+  }
+
+  void _previousPage() {
+    setState(() {
+      _index -= 1;
+    });
+  }
+
+  void _updateAnswer(String updatedValue) {
+    setState(() {
+      _answers = [
+        ..._answers.sublist(0, _index),
+        updatedValue,
+        ..._answers.sublist(_index + 1)
+      ];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextQuestion(question: widget.data[_index]),
-    );
+        child: TextQuestion(
+            question: widget.questions[_index],
+            answer: _answers[_index],
+            previous: _index > 0
+                ? (String updatedValue) {
+                    _previousPage();
+                    _updateAnswer(updatedValue);
+                  }
+                : null,
+            next: _index < widget.questions.length - 1
+                ? (String updatedValue) {
+                    _nextPage();
+                    _updateAnswer(updatedValue);
+                  }
+                : null));
   }
 }
 
 class TextQuestion extends StatefulWidget {
   final String question;
-  const TextQuestion({Key? key, required this.question}) : super(key: key);
+  final String? answer;
+  final void Function(String)? previous;
+  final void Function(String)? next;
+  const TextQuestion(
+      {Key? key, required this.question, this.answer, this.previous, this.next})
+      : super(key: key);
 
   @override
   _TextQuestionState createState() => _TextQuestionState();
 }
 
 class _TextQuestionState extends State<TextQuestion> {
-  late final _textController = TextEditingController();
+  late final _textController = TextEditingController(text: widget.answer);
 
   @override
   void dispose() {
+    debugPrint("hei");
     _textController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(6),
-        child: Column(
-          children: [
-            Text(widget.question),
-            TextField(
-              controller: _textController,
-            ),
-          ],
+    _textController.clear();
+    if (widget.answer != null) {
+      _textController.text = widget.answer!;
+    }
+    return Column(
+      children: [
+        Text(widget.question),
+        TextField(
+          controller: _textController,
         ),
-      ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            widget.previous != null
+                ? TextButton(
+                    onPressed: () => widget.previous!(_textController.text),
+                    child: const Text("Previous"))
+                : Container(),
+            widget.next != null
+                ? TextButton(
+                    onPressed: () => widget.next!(_textController.text),
+                    child: const Text("Next"))
+                : TextButton(onPressed: () {}, child: const Text("Submit")),
+          ],
+        )
+      ],
     );
   }
 }

3b122e8634b7f49b2384538c4f5835d79d062bd3
zixiong2 <zf49@cornell.edu>
2023-04-25 12:22:37 -0400

    build the user history page

---
+++ lib/widgets/history_scroll.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:peeples/models/HistoryModel.dart';
+import 'package:peeples/widgets/history_card.dart';
+
+import '../utils/authentication.dart';
+
+class HistoryListViewState extends ConsumerStatefulWidget {
+  @override
+  _HistoryListViewState createState() => _HistoryListViewState();
+}
+
+class _HistoryListViewState extends ConsumerState<HistoryListViewState> {
+  static const _pageSize = 20;
+
+  final PagingController<int, HistoryModel> _pagingController =
+      PagingController(firstPageKey: 0);
+
+  @override
+  void initState() {
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+    super.initState();
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final newItems = await ref
+          .read(firebaseProvider.notifier)
+          .getHistorys(pageKey, _pageSize);
+      final isLastPage = newItems.length < _pageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => PagedListView<int, HistoryModel>(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        pagingController: _pagingController,
+        builderDelegate: PagedChildBuilderDelegate<HistoryModel>(
+          itemBuilder: (context, item, index) => HistoryCard(history: item),
+        ),
+      );
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    // ref.read(firebaseProvider.notifier).resetLastHistory();
+    super.dispose();
+  }
+}
